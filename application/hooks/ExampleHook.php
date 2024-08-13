f8f2dd36ec2888d6efab545e7c3af8a9b9f3c830
Ali Onder <ali@aralsoft.com>
2024-08-13 13:45:03 +0300

    Put in dummy example files to hooks and third_party directories.

---
+++ application/hooks/ExampleHook.php
@@ -0,0 +1,20 @@
+<?php
+
+class ExampleHook
+{
+    // Constructor function
+    public function __construct()
+    {
+
+
+    }
+
+    // First Class function
+    public function first()
+    {
+
+
+        return TRUE;
+    }
+
+}
